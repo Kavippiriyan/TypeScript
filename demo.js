e175c6863256babafc9090db29c0a6a3ee2e145b
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-13 17:59:56 +0530

    updated typescript

--- demo.js
+++ demo.js
@@ -10,3 +10,18 @@ var arr1 = [true, 2];
 arr1.push(5);
 arr1.push(false);
 console.log(arr1);
+var value;
+value = 5;
+value = "hello";
+console.log(value);
+// ***(objects)***
+var obj = {
+    name: "kavi",
+    age: 22
+};
+console.log(obj);
+console.log(obj.name + " age is " + obj.age);
+var obj1;
+obj1 = { name: "kesavan", age: 22 };
+console.log(obj1);
+console.log(obj1.name + " age is " + obj1.age);
--- demo.ts
+++ demo.ts
@@ -1,19 +1,48 @@
-//   **(Array)**
+// //   **(Array)**
 
 
-let array = [1,2,3,4];
-array.push(5)
-console.log(array);
+// let array = [1, 2, 3, 4];
+// array.push(5)
+// console.log(array);
 
-let arr :string[] = ["Good","Evening"]
+// let arr: string[] = ["Good", "Evening"]
 
-arr.push("All")
-console.log(arr);
+// arr.push("All")
+// console.log(arr);
 
-// **(union type)**
-let arr1 : (number|boolean)[] =[true,2]
+// // **(union type)**
+// let arr1: (number | boolean)[] = [true, 2]
 
-arr1.push(5)
-arr1.push(false)
-console.log(arr1);
+// arr1.push(5)
+// arr1.push(false)
+// console.log(arr1);
+
+
+// let value: string | number
+// value = 5
+// value = "hello"
+
+// console.log(value);
+
+
+// // ***(objects)***
+
+// let obj = {
+//     name:"kavi",
+//     age:22
+// }
+
+// console.log(obj);
+// console.log(obj.name+" age is "+obj.age);
+
+
+
+// let obj1 : {
+//     name: string
+//     age:number
+// }
+
+// obj1 = {name:"kesavan",age:22}
+// console.log(obj1);
+// console.log(obj1.name+" age is "+obj1.age);
 
