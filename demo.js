9c2f5700764738b0893956b4272d56beb7a679d3
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-16 10:28:18 +0530

    updated code 16/04/2024

--- demo.js
+++ demo.js
@@ -1,19 +1,4 @@
 // //   **(Array)**
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 // let array = [1, 2, 3, 4];
 // array.push(5)
 // console.log(array);
@@ -474,45 +459,40 @@ var __extends = (this && this.__extends) || (function () {
 // }
 // let obj = new b(1,2)
 // obj.add()
-var add = /** @class */ (function () {
-    function add() {
-        this.x = 10;
-        this.y = 20;
-    }
-    add.prototype.add = function () {
-        console.log("class  add : ", this.x + this.y);
-    };
-    return add;
-}());
-var add1 = /** @class */ (function (_super) {
-    __extends(add1, _super);
-    function add1() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.x = 20;
-        _this.y = 40;
-        return _this;
-    }
-    add1.prototype.add = function () {
-        console.log("class  add1 : ", this.x + this.y);
-    };
-    return add1;
-}(add));
-var add2 = /** @class */ (function (_super) {
-    __extends(add2, _super);
-    function add2() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.x = 50;
-        _this.y = 50;
-        return _this;
-    }
-    add2.prototype.add = function () {
-        console.log("class  add2 : ", this.x + this.y);
-    };
-    return add2;
-}(add1));
-var obj1 = new add();
-obj1.add();
-var obj2 = new add1();
-obj2.add();
-var obj3 = new add2();
-obj3.add();
+// class add
+// {
+//     x:number = 10
+//     y:number = 20
+//     add():void
+//     {
+//         console.log("class  add : ",this.x+this.y);
+//     }
+// }
+// class add1 extends add
+// { 
+//     x:number = 20
+//     y:number = 40
+//     add():void
+//     {
+//         console.log("class  add1 : ",this.x+this.y);
+//     }
+// }
+// class add2 extends add1
+// { 
+//     x:number = 50
+//     y:number = 50
+//     add():void
+//     {
+//         console.log("class  add2 : ",this.x+this.y);
+//     }
+// }
+// let obj1 = new add()
+// obj1.add()
+// let obj2 = new add1()
+// obj2.add()
+// let obj3 = new add2()
+// obj3.add()
+// var sum = (x:string,y:string)=>x+y
+// console.log(sum("Welcome","Home"));
+var sum = function (x, y) { return x + y; };
+console.log(sum(1, "Home"));
--- demo.ts
+++ demo.ts
@@ -720,53 +720,55 @@
 // obj.add()
 
 
-class add
-{
-    x:number = 10
-    y:number = 20
+// class add
+// {
+//     x:number = 10
+//     y:number = 20
 
-    add():void
-    {
-        console.log("class  add : ",this.x+this.y);
+//     add():void
+//     {
+//         console.log("class  add : ",this.x+this.y);
         
-    }
+//     }
 
     
-}
+// }
 
-class add1 extends add
-{ 
-    x:number = 20
-    y:number = 40
+// class add1 extends add
+// { 
+//     x:number = 20
+//     y:number = 40
 
-    add():void
-    {
-        console.log("class  add1 : ",this.x+this.y);
+//     add():void
+//     {
+//         console.log("class  add1 : ",this.x+this.y);
         
-    }
+//     }
 
     
-}
+// }
 
-class add2 extends add1
-{ 
-    x:number = 50
-    y:number = 50
+// class add2 extends add1
+// { 
+//     x:number = 50
+//     y:number = 50
 
-    add():void
-    {
-        console.log("class  add2 : ",this.x+this.y);
+//     add():void
+//     {
+//         console.log("class  add2 : ",this.x+this.y);
         
-    }
+//     }
 
     
-}
+// }
 
-let obj1 = new add()
-obj1.add()
+// let obj1 = new add()
+// obj1.add()
 
-let obj2 = new add1()
-obj2.add()
+// let obj2 = new add1()
+// obj2.add()
 
-let obj3 = new add2()
-obj3.add()
+// let obj3 = new add2()
+// obj3.add()
+
+
