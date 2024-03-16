cf249abf67af2db2ddf8c3c90c116008955d02ee
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-16 12:12:48 +0530

    updated code 16/04/2024

---
+++ function_overloading.js
@@ -0,0 +1,5 @@
+function add(x, y) {
+    return x + y;
+}
+console.log(add("kavippiriyan", "S"));
+console.log(add(1, 2));
---
+++ function_overloading.ts
@@ -0,0 +1,14 @@
+
+
+function add(x: string, y: string): string
+
+function add(x: number, y: number): number
+
+
+function add(x: any, y: any): any {
+    return x + y
+
+}
+
+console.log(add("kavippiriyan", "S"));
+console.log(add(1, 2));
