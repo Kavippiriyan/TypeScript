ad7833c746d7935b5ea4578730991a09678bb459
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-18 17:46:11 +0530

    updated code 18/04/2024

---
+++ date.js
@@ -0,0 +1,5 @@
+var dateobj = new Date();
+var date = dateobj.getDate();
+var month = dateobj.getMonth();
+var year = dateobj.getFullYear();
+console.log("".concat(date, "/").concat(month, "/").concat(year));
---
+++ date.ts
@@ -0,0 +1,10 @@
+let dateobj = new Date();
+
+let date = dateobj.getDate();
+
+let month = dateobj.getMonth();
+
+let year = dateobj.getFullYear();
+
+
+console.log(`${date}/${month}/${year}`);
